58ebbeb79a079f771cdefbda54bfe8574af7243a
Olaotan Abarowei <olaotanabarowei@gmail.com>
2025-07-11 22:44:39 +0100

    updated paystack keys

--- utils/Paystack.js
+++ utils/Paystack.js
@@ -2,6 +2,12 @@ const Paystack = require("paystack-node");
 
 const environment = process.env.NODE_ENV;
 
-const paystack = new Paystack(process.env.PAYSTACK_SECRET_KEY, environment);
+const isProduction = process.env.NODE_ENV === "production";
+
+const paystackKey = isProduction
+  ? process.env.PAYSTACK_SECRET_KEY_LIVE
+  : process.env.PAYSTACK_SECRET_KEY_TEST;
+
+const paystack = new Paystack(paystackKey, environment);
 
 module.exports = paystack;
